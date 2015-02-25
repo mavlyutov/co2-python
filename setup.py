388b1c8627bd290d60e1ebe8eb9f8c4d74169329
Marat Mavlyutov <mavlyutov@yandex-team.ru>
2015-02-25 16:06:03 +0300

    woopsy doopsy

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setuptools.setup(
     version="1.0",
     description="CO2 Monitor",
     author="Marat Mavlyutov",
-    author_email="mavlyutov@yandex-team.ru",
-    url="http://wiki.yandex-team.ru/",
+    author_email="m.mavlyutov@yandex.ru",
+    url="http://yandex.ru/",
     packages=["co2_monitor"],
     install_requires=["pyusb >= 1.0.0b2"],
     include_package_data=True
